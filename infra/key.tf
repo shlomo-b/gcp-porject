625d83e8aae24a8ec7270caab04dde5768555b6b
shlomob <149378049+shlomo-b@users.noreply.github.com>
2025-06-22 23:37:28 +0300

    update output

--- infra/key.tf
+++ infra/key.tf
@@ -4,10 +4,10 @@ resource "tls_private_key" "ssh" {
 
 resource "local_file" "local_ssh_key" {
   content  = tls_private_key.ssh.private_key_pem
-  filename = "${path.root}/ssh-keys/ssh_key"
+  filename = "${path.root}/keys/ssh_key"
 }
 
 resource "local_file" "local_ssh_key_pub" {
   content  = tls_private_key.ssh.public_key_openssh
-  filename = "${path.root}/ssh-keys/ssh_key.pub"
+  filename = "${path.root}/keys/ssh_key.pub"
 }
--- infra/output.tf
+++ infra/output.tf
@@ -2,11 +2,6 @@ output "gcp_account_used" {
   value = data.google_client_openid_userinfo.user.email
 }
 
-output "vpc" {
-  value = module.vpc
+output "instance_ip" {
+  value = module.instance.external_ip
 }
-
-# output "instance_ip" {
-#   value = module.instance.network_interfaces.0.nat
-  
-# }
